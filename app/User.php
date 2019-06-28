e610159c25974973f2ce6e30cbeecd65a0bf8da2
Erzhan Kalybek <e.kalybek@pointerbp.com>
2019-06-28 16:27:11 +0200

    first commit

---
+++ app/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'linkedin_id', 'linkedin_token', 'avatar',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+}
