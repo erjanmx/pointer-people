11c8bee9224fcbb1c6dec60b0a98489342c1b020
Erzhan Kalybek <e.kalybek@pointerbp.com>
2019-08-09 12:07:10 +0200

    Closable alert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
     @if (session('status'))
-        <div class="alert alert-info">
+        <div class="alert alert-info alert-dismissible">
             {{ session('status') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
     @endif
 
