08bed9f40d5151db4394e4f0ee0cfeaa2dd2656a
erjanmx <erjanmx@gmail.com>
2019-09-30 23:36:45 +0200

    Add meta description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Pointer People is here to help you get a better overview of who your colleagues are" />
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
