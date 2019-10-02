b72eb16308939c2e4f46552bb1d35951d750f9f9
erjanmx <erjanmx@gmail.com>
2019-10-02 09:35:12 +0200

    Add required mark on email label

--- resources/views/profile/form.blade.php
+++ resources/views/profile/form.blade.php
@@ -25,7 +25,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Pointer E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right required">{{ __('Pointer E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 {{ Form::text('email', $user->email, ['class' => 'form-control']) }}
